fc65bfa34113ca504212c3e1b6772b2875780a1a
ismael-miguel <espertalhao04@hotmail.com>
2015-05-04 11:26:06 +0100

    Update quickstyle.css.php

--- quickstyle.css.php
+++ quickstyle.css.php
@@ -16,7 +16,8 @@
 			'shadow'=>true,
 			'sizes'=>true,
 			'styles'=>true,
-			'send_header'=>true
+			'send_header'=>true,
+			'custom'=>null
 		);
 	
 	}
@@ -28,17 +29,18 @@
 	
 	$colors = array(
 		'black',
-		'red'=>array('dark'),
+		'red'=>array('dark','indian','mediumviolet','orange','paleviolet'),
 		'green'=>array('dark','light','forest','yellow','lawn','lime','pale','darkolive','sea','darksea','lightsea','mediumsea','spring','mediumspring'),
+		'blue'=>array('alice','cadet','cornflower','dark','darkslate','deepsky','dodge','light','lightsky','lightsteel','medium','mediumslate','midnight','powder','royal','sky','slate','steel'),
 		'white'
 	);
 	
 	$sizes=array(
-		'em'=>array(0.3,0.5,1,1.3,2,2.5,3),
-		'px'=>array(0,1,2,3,4,5,6,7,8,9,10,12,15,16,17,20),
+		'em'=>array(0.3,0.5,1,1.25,1.3,2,2.5,3,3.3,3.5,4),
+		'px'=>array(0,1,2,3,4,5,6,7,8,9,10,11,12,12.5,13,14,15,16,17,17.5,18,19,20),
 		'mm'=>range(0,9),
 		'cm'=>range(0,9),
-		'%'=>array(0,1,2,3,4,5,10,15,20,25,30,33.3,45,50,55,60,66.6,65,70,75,80,85,90,95,99,100)
+		'%'=>array(0,1,2,3,4,5,6,7,8,9,10,12.5,15,20,25,30,33.3,35,40,45,50,55,60,62.5,65,66.6,65,70,75,80,85,90,95,99,100)
 	);
 	
 	$styles=array(
@@ -51,9 +53,9 @@
 	
 	foreach($colors as $color_name=>$color)
 	{
-		if(is_array($color))
+		if( is_array($color) )
 		{
-			foreach($color as $sub_color)
+			foreach( $color as $sub_color )
 			{				
 				if( isset($config['text']) && $config['text'] )
 				{
@@ -114,6 +116,38 @@
 	}
 	unset($color,$color_name,$sub_color,$sub_color_name);
 	
+	if( isset($config['custom']) )
+	{
+		foreach( $config['custom'] as $color=>$hex )
+		{
+			if( isset($config['text']) && $config['text'] )
+			{
+				echo $class, '.custom-', $color, '-text{color:', $color, $important, ';}';
+			}
+			
+			if( isset($config['border']) && $config['border'] )
+			{
+				echo $class, '.custom-', $color, '-border{border-color:', $color, $important, ';}';
+			}
+			
+			if( isset($config['back']) && $config['back'] )
+			{
+				echo $class, '.custom-', $color, '-back{background-color:', $color, $important, ';}';
+				echo $class, '.custom-', $color, '-background{background-color:', $color, $important, ';}';
+			}
+			
+			if( isset($config['shadow']) && $config['shadow'] )
+			{
+				echo $class, '.custom-', $color, '-shadow{text-shadow-color:', $color, $important, ';box-shadow-color:', $color, $important, ';}';
+			}
+			
+			if( isset($config['outline']) && $config['outline'] )
+			{
+				echo $class, '.custom-', $color, '-outline{outline-color:', $color, $important, ';}';
+			}
+		}
+	}
+	
 	if( isset($config['sizes']) && $config['sizes'])
 	{
 		foreach($sizes as $size_name=>$size_list)
@@ -151,6 +185,3 @@
 		}
 	}
 	unset($style);
-	
-//.border-1px{border-width:1px;}
-//[class*=" border"].solid{border-style:solid;}
